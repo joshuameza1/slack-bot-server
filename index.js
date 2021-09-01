7cdd71cc8fec0473d0b7c39b7c7d8483453eb2cd
Glitch (plump-spiffy-stargazer) <none>
2021-09-01 19:07:59 +0000

    ⛺️🐪 Checkpoint ./index.js:28524505/531 ./routes/routes2.js:28524505/290 ./server/app2.js:28524505/8

--- index.js
+++ index.js
@@ -9,4 +9,18 @@ const config2 = require('./server/configDomain2');
 app.listen(3000, () => console.log(`app listening at http://localhost:3000`));
 
 
-app2.listen(8000, () => console.log("Server listening at port 8000"));
+
+const server2 = require("http").createServer(app2);
+const io = require("socket.io")(server2);
+
+server2.listen(8000, function() {
+  console.log("Server listening at port 8000");
+});
+
+var client = "";
+
+io.on("connection", socket => {
+  console.log("New Client is Connected!");
+  //console.log(socket);
+  client = socket;
+});
--- routes/routes2.js
+++ routes/routes2.js
@@ -1,18 +1,7 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-const server = require("http").createServer(app);
 const router = express.Router();
-
-const io = require("socket.io")(server);
-
-
-io.on("connection", socket => {
-  console.log("New Client is Connected!");
-  //console.log(socket);
-  //client = socket;
-});
 
 module.exports = router;
 
