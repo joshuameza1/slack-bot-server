c4bbbb6f5cc1a4b2467781776a6bda4024ce3bb1
Glitch (plump-spiffy-stargazer) <none>
2021-09-01 18:10:22 +0000

    👷🚕 Checkpoint ./index.js:28524505/1031 ./server/slack_index.js:28524505/42 ./server/socket_index.js:28524505/1011

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const server = require('./server');
+const server = require('./server/slack_index.js');
+
+
 
 server.listen(3000, () => console.log(`app listening at http://localhost:3000`));
+
+
+
+const server2 = require("http").createServer(server2);
+const io = require("socket.io")(sserver2);
+const port = 8000;
+
+sserver2.listen(port, function() {
+  console.log("Server listening at port %d", port);
+});
+
+var client = "";
+
+io.on("connection", socket => {
+  console.log("New Client is Connected!");
+  //console.log(socket);
+  client = socket;
+});
 
