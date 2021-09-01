d754ec25d20ff7b15daf7e3198612b61a342a502
Glitch (plump-spiffy-stargazer) <none>
2021-09-01 19:31:22 +0000

    💠🌛 Checkpoint ./server/app2.js:28524505/1432 ./index.js:28524505/102 ./routes/routes2.js:28524505/11058 ./server/app.js:28524505/618

--- server/app2.js
+++ server/app2.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+
 const httpServer = require("http").createServer(app);
+
 const options = { /* ... */ };
 const io = require("socket.io")(httpServer, options);
 
